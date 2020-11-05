70be98c9d708eab62a3df25df93c6ecfbea0fb7c
Yasuaki Yoshida <yasu5293koh@gmail.com>
2020-11-05 12:43:49 +0900

    [Add] 5章確認問題

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $(function(){
-  $('.box1').hide();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px'
+    }).slideUp();
+  });
 });
