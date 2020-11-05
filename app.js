f182867c1d1a1cf2ed49d500cb2e5bc0ac7b3444
Yasuaki Yoshida <yasu5293koh@gmail.com>
2020-11-05 14:05:20 +0900

    [Add] this,children確認

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+/* 5章確認問題
 $(function(){
   $('.box1').slideDown(function(){
     $('.box1').css({
       'background-color': '#0000FF',
       'height': '100px'
     }).slideUp();
+  });
+});
+*/
+
+/* 6章
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+})
+*/
+
+
+/* 7章　this
+$(function(){
+  $('.box1').on('click', function(){
+    $(this).slideUp();
+  });
+});
+*/
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
